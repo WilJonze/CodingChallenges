8c1a2a42582b804094650c1f5b8fc665e2ba9586
William Berry <wberry264@gmail.com>
2023-02-24 13:52:37 -0500

    Numbers to Strings

---
+++ CC 2023-02-24/app.js
@@ -0,0 +1,13 @@
+// We need a function that can transform a number (integer) into a string.
+// What ways of achieving this do you know?
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+// Practicing again turning numbers into strings. Now we could put the 
+// number into the array and move it around. 
+
+function numberToString(num) {
+    return num.toString();
+  }
